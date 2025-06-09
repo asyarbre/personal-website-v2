2c25ab4f91d79b6b5a28c3bc1ea0f048943902aa
Asya'ri <asyari818@gmail.com>
2025-06-09 13:16:03 +0700

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,40 @@
+# Next.js + Tailwind CSS + TypeScript + Shadcn/UI Starter and Boilerplate
+
+## Features
+This repository is packed with:
+
+- ⚡️ Next.js 15 with App Router
+- ⚛️ React 19
+- ⛑ TypeScript
+- 💨 TailwindCSS
+- 📦 Shadcn/UI
+- 📈 Absolute Import and Path Alias — Import components using @/ prefix
+- 📏 ESLint 9 — To find and fix problems in your code
+- 🐶 Husky — For running scripts before committing
+- 🤖 Conventional Commit Lint — Make sure you & your teammates follow conventional commit
+- 🚫 lint-staged — Run ESLint and Prettier against staged Git files
+- 🔥 Snippets — A collection of useful snippets
+- 🆕 Cursor Rules — Cursor rules for an improved AI coding experience
+
+## Getting Started
+
+### 1. Clone this template 
+Using `create-next-app`
+   ```bash
+   pnpm create next-app -e https://github.com/asyarbre/nextjs-ts-starter nextjs-starter
+   ```
+
+### 2. Install dependencies
+It is encouraged to use **pnpm** so the husky hooks can work properly.
+
+```bash
+pnpm install
+```
+
+### 3. Run the development server
+
+You can start the server using this command:
+
+```bash
+pnpm dev
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,97 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import typescriptEslintEslintPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends('next', 'next/core-web-vitals', 'prettier'),
+  {
+    plugins: {
+      prettier,
+      'unused-imports': unusedImports,
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      camelcase: 'off',
+      'import/prefer-default-export': 'off',
+      'react/jsx-filename-extension': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/no-unused-prop-types': 'off',
+      'react/require-default-props': 'off',
+
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          ts: 'never',
+          tsx: 'never',
+          js: 'never',
+          jsx: 'never',
+        },
+      ],
+
+      'jsx-a11y/anchor-is-valid': [
+        'error',
+        {
+          components: ['Link'],
+          specialLink: ['hrefLeft', 'hrefRight'],
+          aspects: ['invalidHref', 'preferButton'],
+        },
+      ],
+      'no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  ...compat
+    .extends('plugin:@typescript-eslint/recommended', 'prettier')
+    .map((config) => ({
+      ...config,
+      files: ['**/*.+(ts|tsx)'],
+    })),
+  {
+    files: ['**/*.+(ts|tsx)'],
+
+    plugins: {
+      '@typescript-eslint': typescriptEslintEslintPlugin,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-use-before-define': [0],
+      '@typescript-eslint/no-use-before-define': [1],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  },
+];
