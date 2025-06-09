6d428ba66755221ed12d403df24870093fcdbc24
Asya'ri <asyari818@gmail.com>
2025-06-09 14:30:35 +0700

    feat: add Navbar component and icons for various services

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 
 import './globals.css';
 
+import Navbar from '@/components/layouts/navbar';
 import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
+          <Navbar />
         </ThemeProvider>
       </body>
     </html>
