cf45030e976be6b0e45669541bee2fff65ac664e
Asya'ri <asyari818@gmail.com>
2025-06-10 19:59:00 +0700

    feat: add featured projects section with project cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { ChevronRight } from 'lucide-react';
+import Link from 'next/link';
+
 import { BlurFade } from '@/components/magicui/blur-fade';
 import BlurFadeText from '@/components/magicui/blur-fade-text';
+import { ProjectCard } from '@/components/project-card';
 import Skill from '@/components/skills';
 import { Spotlight } from '@/components/spotlight';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
@@ -11,10 +15,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
       <section id="hero">
-        <Spotlight
-          className="-top-40 left-0 md:-top-20 md:left-60"
-          fill="white"
-        />
+        <Spotlight />
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 flex justify-between">
             <div className="flex-col flex flex-1 space-y-1.5">
@@ -56,6 +57,40 @@ export default function Home() {
         <BlurFade delay={BLUR_FADE_DELAY * 6}>
           <Skill />
         </BlurFade>
+      </section>
+      <section id="projects">
+        <BlurFade delay={BLUR_FADE_DELAY * 7}>
+          <div className="flex items-center justify-between">
+            <h2 className="text-xl font-bold">Featured Projects</h2>
+            <Link
+              href={'/projects'}
+              className="flex items-center text-muted-foreground/80 hover:text-muted-foreground"
+            >
+              <p className="font-medium text-sm ">see more projects</p>
+              <ChevronRight className="w-4 h-4" />
+            </Link>
+          </div>
+        </BlurFade>
+        <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto mt-4">
+          {DATA.projects.map((project, id) => (
+            <BlurFade
+              key={project.title}
+              delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+            >
+              {project.isFeatured && (
+                <ProjectCard
+                  href={project.href}
+                  key={project.title}
+                  title={project.title}
+                  description={project.description}
+                  tags={project.technologies}
+                  image={project.image}
+                  links={project.links}
+                />
+              )}
+            </BlurFade>
+          ))}
+        </div>
       </section>
     </main>
   );
